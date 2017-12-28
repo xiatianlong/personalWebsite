2db6696611155aa98585482f89c6b82488e9467e
xiatl <xiatianlong@hotmail.com>
2017-12-28 15:38:32 +0800

    新加公共常量类

---
+++ src/main/java/com/personalWebsite/common/system/Constant.java
@@ -0,0 +1,14 @@
+package com.personalWebsite.common.system;
+
+/**
+ * 系统常量
+ * Created by xiatianlong on 2017/12/28.
+ */
+public class Constant {
+
+    /**
+     * 管理员名称
+     */
+    public static final String ADMIN = "admin";
+
+}
