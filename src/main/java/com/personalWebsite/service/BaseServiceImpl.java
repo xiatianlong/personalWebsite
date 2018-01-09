db9b0b420999a867eb07d709b348e8b3fb54d98a
xiatl <xiatianlong@hotmail.com>
2018-01-09 16:26:29 +0800

    添加获取session用户信息方法

--- src/main/java/com/personalWebsite/service/BaseServiceImpl.java
+++ src/main/java/com/personalWebsite/service/BaseServiceImpl.java
@@ -1,14 +1,20 @@
 package com.personalWebsite.service;
 
+import com.personalWebsite.vo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 /**
  * Created by xiatianlong on 2017/12/27.
  */
 public class BaseServiceImpl implements BaseService {
+
+    @Autowired
+    private HttpServletRequest request;
+
 
     @Autowired
     protected MessageSource messageSource;
@@ -33,6 +39,15 @@ public class BaseServiceImpl implements BaseService {
      */
     protected String getMessage(String messageKey, Object[] args) {
         return messageSource.getMessage(messageKey, args, Locale.CHINA);
+    }
+
+    /**
+     * 获取登录用户信息
+     *
+     * @return userInfo
+     */
+    protected UserInfo getLoinUser() {
+        return (UserInfo) request.getSession().getAttribute("LOGIN_USER");
     }
 
 }
