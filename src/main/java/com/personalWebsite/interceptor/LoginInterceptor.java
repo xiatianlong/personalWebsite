3814d32d5a8b56b83a8ccd6947bbf5fa58be3889
xiatl <xiatianlong@hotmail.com>
2018-01-09 16:27:52 +0800

    登录拦截

---
+++ src/main/java/com/personalWebsite/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.personalWebsite.interceptor;
+
+import com.personalWebsite.common.exception.ApplicationException;
+import com.personalWebsite.vo.UserInfo;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 请求是否登录拦截器
+ * Created by xiatianlong on 2018/1/8.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        UserInfo userInfo = (UserInfo) request.getSession().getAttribute("LOGIN_USER");
+        if (userInfo == null) {
+            throw new ApplicationException("请登录");
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
