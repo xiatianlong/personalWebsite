479e7104117f69e3e30565bcaded8a53dfa89a4f
xiatl <xiatianlong@hotmail.com>
2018-01-19 13:52:58 +0800

    添加request监听器

--- src/main/java/com/personalWebsite/controller/BaseController.java
+++ src/main/java/com/personalWebsite/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.personalWebsite.controller;
 
+import com.personalWebsite.vo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
@@ -7,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,6 +18,9 @@ import java.util.Locale;
  * Created by xiatianlong on 2016/12/30.
  */
 public class BaseController {
+
+    @Autowired
+    private HttpServletRequest request;
 
     /**
      * 数据绑定
@@ -52,6 +57,20 @@ public class BaseController {
      */
     protected String getMessage(String messageKey, Object[] args) {
         return messageSource.getMessage(messageKey, args, Locale.CHINA);
+    }
+
+    /**
+     * 获取登录用户信息
+     *
+     * @return userInfo
+     */
+    protected UserInfo getLoinUser() {
+        UserInfo userInfo = (UserInfo) request.getSession().getAttribute("LOGIN_USER");
+        if (userInfo == null) {
+            // 创建空的UserInfo对象，防止空指针
+            userInfo = new UserInfo();
+        }
+        return userInfo;
     }
 
 }
