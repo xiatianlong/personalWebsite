17c37e06e064767a22da4fce50f0c350978d2469
xiatl <xiatianlong@hotmail.com>
2018-03-12 15:38:03 +0800

    注释提交

--- src/main/java/com/personalWebsite/controller/admin/AdminIndexController.java
+++ src/main/java/com/personalWebsite/controller/admin/AdminIndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * 首页 controller.
  * Created by xiatianlong on 2018/2/28.
  */
 @Controller
--- src/main/java/com/personalWebsite/controller/admin/AdminNoteController.java
+++ src/main/java/com/personalWebsite/controller/admin/AdminNoteController.java
@@ -19,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * 后台笔记 controller.
  * Created by xiatianlong on 2018/2/28.
  */
 @Controller
